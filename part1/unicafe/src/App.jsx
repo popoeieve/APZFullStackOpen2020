23ad9d5f54f94faef0cc80d4881b975f5ce48fe7
popoeieve <midgarzone@hotmail.com>
2024-05-10 15:40:59 +0200

    exercise 1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,6 +14,30 @@ const ListUsed = (props) => (
   </div>
 )
 
+const Total = (props) => {
+  const total = props.good+props.neutral+props.bad
+  return(
+    <p>All {total}</p>
+  )
+
+}
+
+const Average = (props) => {
+  const average = (props.good+props.bad)/(props.good+props.neutral+props.bad)
+  return(
+    <p>All {average}</p>
+  )
+
+}
+
+const Positive = (props) => {
+  const positive = props.good/(props.good+props.neutral+props.bad)*100
+  return(
+    <p>Positive {positive}</p>
+  )
+
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
@@ -30,6 +54,9 @@ const App = () => {
       <ListUsed name={'good'} timesUsed={good}></ListUsed>
       <ListUsed name={'neutral'} timesUsed={neutral}></ListUsed>
       <ListUsed name={'bad'} timesUsed={bad}></ListUsed>
+      <Total good={good} neutral={neutral} bad={bad}></Total>
+      <Average good={good} neutral={neutral} bad={bad}></Average>
+      <Positive good={good} neutral={neutral} bad={bad}></Positive>
     </div>
   )
 }
