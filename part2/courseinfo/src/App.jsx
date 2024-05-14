1a2e75e21d7826ec333758e58283824a7ef12582
popoeieve <midgarzone@hotmail.com>
2024-05-14 12:54:06 +0200

    exercise 2.4 done

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,7 +1,5 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-//const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
 const Part = ({ part }) => 
   <p>
     {part.name} {part.exercises}
@@ -20,43 +18,69 @@ const Content = ({ parts }) => {
   );
 }
 
-const Course=({course,parts})=>{
+const Course=({courses})=>{
+  var courseList=courses.map((course,id) => (
+    <>
+      <Header course={course.name}/>
+      <Content key={id} parts={course.parts}/>
+    </>
+  ));
   return(
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
+      {courseList}
     </div>
-  )
+  );
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const courses = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
     {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    },
-    {
-      name: 'Extra',
-      exercises: 12
-    },
-    {
-      name: 'Test',
-      exercises: 15
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
   ]
 
   return (
     <div>
-      <Course course={course} parts={parts}/>
+      <Course courses={courses}/>
     </div>
   )
 }
